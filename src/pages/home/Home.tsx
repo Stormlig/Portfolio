2cb76c5e2e5fa0c15eca393599a602918b65da9e
Stormlig <114263838+Stormlig@users.noreply.github.com>
2024-02-17 11:38:16 -0300

    add components

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,7 @@
 import About from '../../components/background/About'
 import BallAnimationWithToolsBadge from '../../components/background/BallAnimationWithToolsBadge'
+import Contact from '../../components/background/Contact'
+import Footer from '../../components/background/Footer'
 import Header from '../../components/background/Header'
 import Project from '../../components/background/Project'
 
@@ -13,6 +15,10 @@ const Home = () => {
       <BallAnimationWithToolsBadge />
       <span className='w-[100%]   h-[8px] bg-slate-400 roubded-sm'></span>
       <Project />
+      <span className='w-[100%]   h-[8px] bg-slate-400 roubded-sm'></span>
+      <Contact />
+      <span className='w-[100%]   h-[8px] bg-slate-400 roubded-sm'></span>
+      <Footer />
     </main>
   )
 }
