8bd74f9982bf7642160ccbaed772da7657f71122
Stormlig <114263838+Stormlig@users.noreply.github.com>
2024-02-20 10:45:19 -0300

    update nomeclature

--- src/components/background/Project/data/allProjects.ts
+++ src/components/background/Project/data/allProjects.ts
@@ -1,39 +1,31 @@
 export const allProjects = [
   {
     title: 'Devorum',
-    context: 'Ambiente de comunicação assíncrona, voltado para duvidas sobre tecnologia e desenvolvimento',
-    media: '/images/sobre_devorum.webp',
+    description: 'Ambiente de comunicação assíncrona, voltado para duvidas sobre tecnologia e desenvolvimento',
+    media: '/images/devorum-x.png',
     link: 'https://devorum.vercel.app/',
     devTools: ''
   },
   {
     title: 'Smart Manager',
-    context: 'Gerenciador de serviços, desenvolvido para facilitar a gestão do comercio',
-    media: '/images/smart_manager.png',
+    description: 'Gerenciador de serviços, desenvolvido para facilitar a gestão do comercio',
+    media: '/images/smart-manager.png',
     link: 'https://github.com/Vortex-Technology/SmartManager-HJV-Electron',
     devTools: ''
 
   },
   {
     title: 'Game-Car',
-    context: 'Esse projeto foi desenvolvido com o intuito de adquirir conhecimentos no frontend, principalmente na questão logica voltada para o mundo dos games, para que fosse possível criar meu porfolio em 3D',
-    media: '',
-    link: 'https://portfolio-game-mu-93.vercel.app/',
-    devTools: ''
-
-  },
-  {
-    title: 'E-commerce',
-    context: 'mercado',
-    media: '',
-    link: 'https://e-commerce-six-red.vercel.app/',
+    description: 'Esse projeto foi desenvolvido com o intuito de adquirir conhecimentos no frontend, principalmente na questão logica voltada para o mundo dos games, para que fosse possível criar meu porfolio em 3D',
+    media: '/images/game-car.png',
+    link: 'https://game-car-mu.vercel.app/',
     devTools: ''
 
   },
   {
     title: 'Projecto-Website',
-    context: '',
-    media: '',
+    description: 'Primeiro projeto que fiz, quando decidir me tornar um desenvolvedor',
+    media: '/images/projeto.png',
     link: 'https://stormlig.github.io/Projeto-Website/index.html#',
     devTools: ''
 
