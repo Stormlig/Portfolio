99de46b1e9a8e2dfbdd577a5f8907a469d8624c5
Stormlig <114263838+Stormlig@users.noreply.github.com>
2024-02-22 16:05:46 -0300

    refactor style and nomeclature

--- src/components/background/Header/components/Nav.tsx
+++ src/components/background/Header/components/Nav.tsx
@@ -1,23 +1,23 @@
-import FooterNav from './NavFloating'
+import NavFloating from './NavFloating'
 
 const Nav = () => {
   return (
     <>
-      <FooterNav />
-      <nav className="w-[100%] sticky top-0 h-[12vh] bg-[#141c27] shadow-md z-10">
+      <NavFloating />
+      <nav className="fixed w-[100%] pt-8 pb-6 -top-2  md:pt-14 md:pb-12 bg-[#141c27] shadow-md z-10">
         <ul className="flex items-center justify-between w-[80%] mx-auto h-full" >
 
-          <h1 className="flex-[0.6] cursor-pointer text-[25px] text-white font-primary">
+          <h1 className="flex-[0.8] cursor-pointer text-[25px] md:text-[30px] text-white font-primary">
             Herrison
             <span className="text-yellow-300">Souza</span>
           </h1>
 
           <li className="nav-link"><a href="#home">Início</a></li>
           <li className="nav-link"><a href="#about">Sobre mim</a></li>
-          <li className="nav-link"><a href="#skills">Ferramentas</a></li>
+          {/* <li className="nav-link"><a href="#skills">Ferramentas</a></li> */}
           <li className="nav-link"><a href="#project">Projetos</a></li>
           <li className="nav-link"><a href="#contact">Contato</a></li>
-          <li className="nav-link"><a href="#home3d">Versão 3D</a></li>
+          {/* <li className="nav-link"><a href="#home3d">Versão 3D</a></li> */}
 
         </ul>
       </nav>
--- src/components/background/Header/components/NavFloating.tsx
+++ src/components/background/Header/components/NavFloating.tsx
@@ -1,17 +1,39 @@
+import { useState, useLayoutEffect, memo } from 'react'
+
 const FooterNav = () => {
   return (
     <nav className="fixed bottom-0 left-0 w-full bg-[#141c27] opacity-90 shadow-md text-white z-10">
       <ul className="flex justify-between mx-auto py-4 w-[80%]">
-
         <li className="nav-link-floating"><a href="#home">Início</a></li>
         <li className="nav-link-floating"><a href="#about">Sobre</a></li>
-        <li className="nav-link-floating"><a href="#skills">Ferramentas</a></li>
         <li className="nav-link-floating"><a href="#project">Projetos</a></li>
         <li className="nav-link-floating"><a href="#contact">Contato</a></li>
-        {/* <li className="nav-link-floating"><a href="#home3d">Versão 3D</a></li> */}
       </ul>
     </nav>
   )
 }
 
-export default FooterNav
+const NavFloating = () => {
+  const [showNav, setShowNav] = useState(true)
+
+  useLayoutEffect(() => {
+    const handleResize = () => {
+      const screenWidth = window.innerWidth
+      setShowNav(screenWidth < 768)
+    }
+
+    handleResize() // Verifica o tamanho da tela imediatamente após a renderização do componente
+
+    // Adiciona um ouvinte de redimensionamento para atualizar o estado quando a tela for redimensionada
+    window.addEventListener('resize', handleResize)
+
+    // Retorna uma função de limpeza para remover o ouvinte de redimensionamento quando o componente for desmontado
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, []) // O array vazio garante que o efeito só seja executado uma vez
+
+  return showNav ? <FooterNav /> : null
+}
+
+export default memo(NavFloating)
