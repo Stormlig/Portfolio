a138fb2382bcb9cbceae6d2879cac1a3ced0b3e9
Stormlig <114263838+Stormlig@users.noreply.github.com>
2024-02-23 12:43:55 -0300

    update href for const

--- src/components/background/Footer/Footer.tsx
+++ src/components/background/Footer/Footer.tsx
@@ -1,19 +1,20 @@
 import { SiDiscord, SiGithub, SiLinkedin } from 'react-icons/si'
-
+import { github, linkedin } from '../../../utils/socials'
+import { FaRegCopyright } from 'react-icons/fa'
 const Footer = () => {
   return (
-    <footer className=" pb-20 pt-8 bg-[#02050a] text-white ">
+    <footer className=" pb-20 pt-8 bg-[#02050a] text-white xl:py-10">
       <div className='flex flex-row-auto justify-around '>
-        <p className='text-[20px]'>@2024 <span className='text-yellow-300 '> Herrison </span></p>
+        <p className='text-[20px] flex'><FaRegCopyright className='w-[1.6rem] h-[2rem]' /> 2024 <span className='text-yellow-300 '> Herrison </span></p>
         <ul className='flex flex-row text-[2rem] '>
           <li className='mx-2 hover:text-yellow-100 duration-300'>
-            <a href="https://www.linkedin.com/in/herrison-souza-630118187/">
+            <a href={linkedin}>
               <SiLinkedin />
             </a>
           </li>
 
           <li className='mx-2 hover:text-yellow-100 duration-300'>
-            <a href="https://github.com/Stormlig">
+            <a href={github}>
               <SiGithub />
             </a>
           </li>
