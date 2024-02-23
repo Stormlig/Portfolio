dbc89ceaa792b237f4217c26ba88947b011f6bd2
Stormlig <114263838+Stormlig@users.noreply.github.com>
2024-02-23 12:41:43 -0300

    to fill in info field

--- src/utils/socials.ts
+++ src/utils/socials.ts
@@ -1,3 +1,3 @@
-export const linkedin = ''
-export const github = ''
+export const linkedin = 'https://www.linkedin.com/in/herrison-souza-630118187/'
+export const github = 'https://github.com/Stormlig'
 export const discord = ''
