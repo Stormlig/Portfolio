7d743e620dbc498cf808f2310783037af9ae1a1f
Stormlig <114263838+Stormlig@users.noreply.github.com>
2024-02-24 15:50:17 -0300

    fixed bug

--- src/components/background/Hero/Hero.tsx
+++ src/components/background/Hero/Hero.tsx
@@ -15,26 +15,23 @@ const Hero = memo(() => {
         <HexaneParticles />
       </div>
 
-      <div className=' z-10'>
-        <h1 className='text-[28px] md:text-[40px] text-white '>
-          Olá, eu sou o <span className='text-yellow-300'>Herrison</span>
-        </h1>
+      <h1 className='text-[28px] md:text-[40px] text-white '>
+        Olá, eu sou o <span className='text-yellow-300'>Herrison</span>
+      </h1>
 
-        <p className='w-[80%] mt-[2rem] mb-[1rem] text-[20px] md:text-[26px] md:w-[80%] lg:w-[50%] text-[#ffffffdd] max-sm:w-3/4 italic font-semibold'>
-          Desbravando este vasto mundo, cheio de mistérios e desafios, busco refinar cada dia meus conhecimentos como:
-        </p>
+      <p className='w-[80%] mt-[2rem] mb-[1rem] text-[20px] md:text-[26px] md:w-[80%] lg:w-[50%] text-[#ffffffdd] max-sm:w-3/4 italic font-semibold'>
+        Desbravando este vasto mundo, cheio de mistérios e desafios, busco refinar cada dia meus conhecimentos como:
+      </p>
 
-        <TextEffect />
+      <TextEffect />
 
-        <div className='mt-12 flex-col space-y-6 sm:space-y-0 sm:flex sm:flex-row items-center sm:space-x-6'>
-          <button className='px-8 md:text-[24px] md:space-x-4 hover:bg-yellow-400 transition-all duration-200 py-4 text-[18px] font-bold uppercase bg-[#55e6a5] text-black flex items-center space-x-3 '>
-            <p>Download CV</p>
-            <LuDownload className='w-[1.6rem] h-[1.7rem]' />
-          </button>
-        </div>
+      <div className='mt-12 flex-col space-y-6 sm:space-y-0 sm:flex sm:flex-row items-center sm:space-x-6'>
+        <button className='px-8 md:text-[24px] md:space-x-4 hover:bg-yellow-400 transition-all duration-200 py-4 text-[18px] font-bold uppercase bg-[#55e6a5] text-black flex items-center space-x-3 '>
+          <p>Download CV</p>
+          <LuDownload className='w-[1.6rem] h-[1.7rem]' />
+        </button>
       </div>
     </section >
-
   )
 })
 
