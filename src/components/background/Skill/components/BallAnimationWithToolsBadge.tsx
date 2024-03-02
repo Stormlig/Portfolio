7be2ba4dca0a627955cc07327bde65ed8bb87964
Stormlig <114263838+Stormlig@users.noreply.github.com>
2024-03-02 12:42:54 -0300

    remove ruby icon

--- src/components/background/Skill/components/BallAnimationWithToolsBadge.tsx
+++ src/components/background/Skill/components/BallAnimationWithToolsBadge.tsx
@@ -14,7 +14,6 @@ import {
   SiNodedotjs,
   SiPostgresql,
   SiReact,
-  SiRuby,
   SiTailwindcss,
   SiTypescript
 } from 'react-icons/si'
@@ -32,7 +31,6 @@ const iconsWithColors = [
   { Icon: SiJest, color: '#C21325' },
   { Icon: SiExpress, color: '#000000' },
   { Icon: SiElectron, color: '#47848F' },
-  { Icon: SiRuby, color: '#CC342D' },
   { Icon: SiGithub, color: '#181717' },
   { Icon: SiTypescript, color: '#3178C6' },
   { Icon: SiGit, color: '#F05032' }
@@ -41,7 +39,7 @@ const iconsWithColors = [
 const BallAnimationWithToolsBadge = () => {
   const memoizedIcons = useMemo(() => {
     return iconsWithColors.map(({ Icon, color }, index) => (
-      <div key={index} className='rounded-full w-[100px] h-[100px] p-2 -m-4 bg-[#ffffffdb] '>
+      <div key={index} className='rounded-full w-[100px] h-[100px] p-2 -m-4 bg-[#ffffffaa] '>
         <Icon size={60} style={{ color }} className='ml-3 mt-3' />
       </div>
     ))
@@ -49,7 +47,11 @@ const BallAnimationWithToolsBadge = () => {
 
   return (
     <div id='skills' className='flex flex-col justify-center items-center w-[80%] my-20 mx-auto '>
-      <h1 className='text-[#b6e655] text-[45px] md:text-[55px] text-center mb-28 font-bold'>Ferramentas</h1>
+      <h1 className='text-[#b6e655] text-[45px] md:text-[55px] text-center mb-28 font-bold'>
+        Ferramentas<br />
+        <span className='text-gray-400 text-[26px]'>( Mas não todas )</span>
+      </h1>
+
       <div className='grid grid-cols-3 max-[400px]:grid-cols-2 md:grid-cols-4 lg:grid-cols-6  items-center gap-[4rem]  text-white'>
         {memoizedIcons}
       </div>
