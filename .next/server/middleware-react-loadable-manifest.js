a353c7c3e759ed468e16b5c2f0e67368e274c39e
Stormlig <herrison.thor@gmail.com>
2026-06-19 12:49:13 -0300

    fix: remove .next from git, add to .gitignore, fix Node.js version to 22.x

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST='{"..\\\\node_modules\\\\next\\\\dist\\\\client\\\\index.js -> ../pages/_app":{"id":5139,"files":["static/chunks/139.7a5a8e93a21948c1.js"]},"..\\\\node_modules\\\\next\\\\dist\\\\client\\\\index.js -> ../pages/_error":{"id":1646,"files":["static/chunks/646.f342b7cffc01feb0.js"]}}';
--- .next/static/chunks/app/layout-c225a02ad036cf25.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{3673:()=>{},4951:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,3673,23))}},e=>{e.O(0,[978,441,255,358],()=>e(e.s=4951)),_N_E=e.O()}]);
